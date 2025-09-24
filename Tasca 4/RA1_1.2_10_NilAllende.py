a83d2fe4983d3c5f099b053d35fc6ca628d75faf
NilAlle <nilallendesole@gmail.com>
2025-09-24 09:50:49 +0200

    Add files via upload

---
+++ Tasca 4/RA1_1.2_10_NilAllende.py
@@ -0,0 +1,13 @@
+num1 = int(input("introdueix un numero "))
+signe = input("tria l'operaci´que vols realitar (+, -, *, /) ")
+num2 = int(input("introdueix un numero "))
+
+if (signe == "+"):
+    print(num1 + num2)
+elif (signe == "-"):
+    print(num1 - num2)
+elif (signe == "*"):
+    print(num1 * num2)
+elif (signe == "/"):
+    print(num1 / num2)
+
---
+++ Tasca 4/RA1_1.2_9_NilAllende.py
@@ -0,0 +1,9 @@
+numero = input("introdueix un número ")
+if (numero < 0):
+    print("és negatiu")
+elif (numero > 0):
+    print("és positiu")
+else:
+    print("és 0")
+
+print("Gràcies!""")
